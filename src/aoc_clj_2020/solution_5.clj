dfcd3a31b80a1217fb7b6db849219e1642c92de8
Christoph Frick <cf@ofnir.net>
2020-12-05 17:40:29 +0100

    Boarding passes are just a convoluted way to write a binary number

--- src/aoc_clj_2020/solution_5.clj
+++ src/aoc_clj_2020/solution_5.clj
@@ -1,47 +1,21 @@
 (ns aoc-clj-2020.solution-5
   (:require [aoc-clj-2020.util.input :as li]
             [aoc-clj-2020.util.parse :as lp]
-            [aoc-clj-2020.util.test :as lt]))
+            [aoc-clj-2020.util.test :as lt]
+            [clojure.string :as str]))
 
-; TODO: is there a way to do this just with bits?
-(defn pick
-  [[lo hi] pick]
-  (let [d (bit-shift-right (- hi lo) 1)
-        c (+ lo d)]
-    (if (= pick :lo)
-      [lo c]
-      [(inc c) hi])))
-
-(def row-col-offsets
-  {\F 0
-   \B 0
-   \L 1
-   \R 1})
-
-(def upper-lower-config
-  {\F :lo
-   \B :hi
-   \L :lo
-   \R :hi})
-
-(defn parse-boarding-pass
-  [boarding-pass-s]
-  (loop [[c & cs] boarding-pass-s
-         row-col [[0 127] [0 7]]]
-    (let [row-col' (update row-col (row-col-offsets c) pick (upper-lower-config c))]
-      (if (seq cs)
-        (recur cs row-col')
-        (mapv first row-col')))))
-
-(defn seat-id
-  [[row col]]
-  (+ (* row 8) col))
+(defn boarding-pass-to-seat-id
+  [boarding-pass]
+  (->> (Integer/parseInt
+        (str/join
+         (map {\F "0" \B "1" \L "0" \R "1"} boarding-pass))
+        2)))
 
 (defn load-seat-ids
   [file-name]
   (->> file-name
        (li/read-lines)
-       (into (sorted-set) (map (comp seat-id parse-boarding-pass)))))
+       (into (sorted-set) (map boarding-pass-to-seat-id))))
 
 (defn part-1
   []
--- test/aoc_clj_2020/test_solution_5.clj
+++ test/aoc_clj_2020/test_solution_5.clj
@@ -3,8 +3,8 @@
             [aoc-clj-2020.solution-5 :as t]))
 
 (deftest test-parse-boarding-pass
-  (are [bp r] (= r (t/parse-boarding-pass bp))
-    "FBFBBFFRLR" [44 5]))
+  (are [bp r] (= r (t/boarding-pass-to-seat-id bp))
+    "FBFBBFFRLR" 357))
 
 (deftest test-solution-5
   (are [f r] (= (f) r)
