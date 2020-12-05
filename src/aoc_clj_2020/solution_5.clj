115554fb327ae47a4aaebe05c604e0817968dc17
Christoph Frick <cf@ofnir.net>
2020-12-05 15:17:07 +0100

    Day 5, part 1

--- src/aoc_clj_2020/solution_5.clj
+++ src/aoc_clj_2020/solution_5.clj
@@ -3,9 +3,45 @@
             [aoc-clj-2020.util.parse :as lp]
             [aoc-clj-2020.util.test :as lt]))
 
+; TODO: is there a way to do this just with bits?
+(defn partition
+  [[lo hi] pick]
+  (let [d (bit-shift-right (- hi lo) 1)
+        c (+ lo d)]
+    (if (= pick :lo)
+      [lo c]
+      [(inc c) hi])))
+
+(def row-col-offsets
+  {\F 0
+   \B 0
+   \L 1
+   \R 1})
+
+(def upper-lower-config
+  {\F :lo
+   \B :hi
+   \L :lo
+   \R :hi})
+
+(defn parse-boarding-pass
+  [boarding-pass-s]
+  (loop [[c & cs] boarding-pass-s
+         row-col [[0 127] [0 7]]]
+    (let [row-col' (update row-col (row-col-offsets c) partition (upper-lower-config c))]
+      (if (seq cs)
+        (recur cs row-col')
+        (mapv first row-col')))))
+
+(defn seat-id
+  [[row col]]
+  (+ (* row 8) col))
+
 (defn part-1
   []
-  nil)
+  (->> (li/read-lines "5.txt")
+       (map (comp seat-id parse-boarding-pass))
+       (apply max)))
 
 (defn part-2
   []
--- test/aoc_clj_2020/test_solution_5.clj
+++ test/aoc_clj_2020/test_solution_5.clj
@@ -2,7 +2,11 @@
   (:require [clojure.test :refer [deftest testing is are]]
             [aoc-clj-2020.solution-5 :as t]))
 
+(deftest test-parse-boarding-pass
+  (are [bp r] (= r (t/parse-boarding-pass bp))
+    "FBFBBFFRLR" [44 5]))
+
 (deftest test-solution-5
   (are [f r] (= (f) r)
-    t/part-1 nil
+    t/part-1 883
     t/part-2 nil))
