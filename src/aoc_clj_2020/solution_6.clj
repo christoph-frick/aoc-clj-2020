b0371672dab40efa8d987154b021a65a5cab3c4e
Christoph Frick <cf@ofnir.net>
2020-12-06 12:53:24 +0100

    Day 6, part 2

--- src/aoc_clj_2020/solution_6.clj
+++ src/aoc_clj_2020/solution_6.clj
@@ -2,27 +2,44 @@
   (:require [aoc-clj-2020.util.input :as li]
             [aoc-clj-2020.util.parse :as lp]
             [aoc-clj-2020.util.test :as lt]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.set :as set]))
 
 (defn parse-answers
   [s]
   (->> (str/split s #"\n\n")
-       (mapv (comp
-              (partial into #{})
-              #(str/replace % "\n" "")))))
+       (mapv (fn [s]
+               (mapv (partial into #{})
+                     (str/split s #"\n"))))))
+
+(defn group-answers
+  [set-op answers]
+  (mapv (partial apply set-op) answers))
+
+(defn group-answers-union
+  [answers]
+  (group-answers set/union answers))
+
+(defn group-answers-intersection
+  [answers]
+  (group-answers set/intersection answers))
 
 (defn sum-answers
-  [s]
-  (->> s
-       (parse-answers)
+  [answers]
+  (->> answers
        (map count)
        (apply +)))
 
 (defn part-1
   []
   (->> (li/read-input "6.txt")
+       (parse-answers)
+       (group-answers-union)
        (sum-answers)))
 
 (defn part-2
   []
-  nil)
+  (->> (li/read-input "6.txt")
+       (parse-answers)
+       (group-answers-intersection)
+       (sum-answers)))
--- test/aoc_clj_2020/test_solution_6.clj
+++ test/aoc_clj_2020/test_solution_6.clj
@@ -21,16 +21,35 @@ b")
 
 (deftest test-parse-answers
   (is (= (t/parse-answers example)
-         [#{\a \b \c}
-          #{\a \b \c}
-          #{\a \b \c}
-          #{\a}
-          #{\b}])))
+         [[#{\a \b \c}]
+          [#{\a}
+           #{\b}
+           #{\c}]
+          [#{\a \b}
+           #{\a \c}]
+          [#{\a}
+           #{\a}
+           #{\a}
+           #{\a}]
+          [#{\b}]])))
 
-(deftest test-sum-answers
-  (is (= 11 (t/sum-answers example))))
+(deftest test-group-answers
+  (are [f r] (= r (f (t/parse-answers example)))
+    t/group-answers-union
+    [#{\a \b \c}
+     #{\a \b \c}
+     #{\a \b \c}
+     #{\a}
+     #{\b}]
+
+    t/group-answers-intersection
+    [#{\a \b \c}
+     #{}
+     #{\a}
+     #{\a}
+     #{\b}]))
 
 (deftest test-solution-6
   (are [f r] (= (f) r)
     t/part-1 6291
-    t/part-2 nil))
+    t/part-2 3052))
