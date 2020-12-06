a48edab9196bb23f48f13b3749d62efa0b2e9a46
Christoph Frick <cf@ofnir.net>
2020-12-06 14:29:56 +0100

    Use kaocha as test runner

--- project.clj
+++ project.clj
@@ -5,4 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/test.check "0.9.0"]
-                 [org.clojure/math.combinatorics "0.1.6"]])
+                 [org.clojure/math.combinatorics "0.1.6"]]
+  :profiles {:kaocha {:dependencies [[lambdaisland/kaocha "1.0.732"]]}}
+  :aliases {"test-refresh" ["kaocha" "--watch"]
+            "kaocha" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]})
