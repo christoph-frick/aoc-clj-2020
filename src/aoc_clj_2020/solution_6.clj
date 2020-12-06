e3283523c16254597c6cdbdbc8e1378c6812cd40
Christoph Frick <cf@ofnir.net>
2020-12-06 12:27:00 +0100

    Day 6, part 1

--- src/aoc_clj_2020/solution_6.clj
+++ src/aoc_clj_2020/solution_6.clj
@@ -1,11 +1,27 @@
 (ns aoc-clj-2020.solution-6
   (:require [aoc-clj-2020.util.input :as li]
             [aoc-clj-2020.util.parse :as lp]
-            [aoc-clj-2020.util.test :as lt]))
+            [aoc-clj-2020.util.test :as lt]
+            [clojure.string :as str]))
+
+(defn parse-answers
+  [s]
+  (->> (str/split s #"\n\n")
+       (mapv (comp
+              (partial into #{})
+              #(str/replace % "\n" "")))))
+
+(defn sum-answers
+  [s]
+  (->> s
+       (parse-answers)
+       (map count)
+       (apply +)))
 
 (defn part-1
   []
-  nil)
+  (->> (li/read-input "6.txt")
+       (sum-answers)))
 
 (defn part-2
   []
--- test/aoc_clj_2020/test_solution_6.clj
+++ test/aoc_clj_2020/test_solution_6.clj
@@ -2,7 +2,35 @@
   (:require [clojure.test :refer [deftest testing is are]]
             [aoc-clj-2020.solution-6 :as t]))
 
+(def example
+  "abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b")
+
+(deftest test-parse-answers
+  (is (= (t/parse-answers example)
+         [#{\a \b \c}
+          #{\a \b \c}
+          #{\a \b \c}
+          #{\a}
+          #{\b}])))
+
+(deftest test-sum-answers
+  (is (= 11 (t/sum-answers example))))
+
 (deftest test-solution-6
   (are [f r] (= (f) r)
-    t/part-1 nil
+    t/part-1 6291
     t/part-2 nil))
