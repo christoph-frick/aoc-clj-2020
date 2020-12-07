24b4822da2d2c01b7e9255a2273fddb3155510e5
Christoph Frick <cf@ofnir.net>
2020-12-07 14:27:18 +0100

    Day 7, part 1

--- src/aoc_clj_2020/solution_7.clj
+++ src/aoc_clj_2020/solution_7.clj
@@ -1,11 +1,54 @@
 (ns aoc-clj-2020.solution-7
   (:require [aoc-clj-2020.util.input :as li]
             [aoc-clj-2020.util.parse :as lp]
-            [aoc-clj-2020.util.test :as lt]))
+            [aoc-clj-2020.util.test :as lt]
+            [clojure.string :as str]))
+
+(defn parse-line
+  [s]
+  (let [[_ bag] (re-find #"^(\w+ \w+) bags? contain" s)
+        contains (re-seq #"(\d+) (\w+ \w+) bags?" s)]
+    {:bag bag
+     :contain (mapv (fn [[_ n b]]
+                      {:bag b
+                       :amount (lp/atoi n)})
+                    contains)}))
+
+(defn parse-lines
+  [s]
+  (->> (str/split s #"\n")
+       (mapv parse-line)))
+
+(defn to-contained
+  [lines]
+  (let [assoc-set (fn [m k v]
+                    (if (contains? m k)
+                      (update m k conj v)
+                      (assoc m k #{v})))]
+    (reduce (fn [m {:keys [bag contain]}]
+              (reduce (fn [m {c-bag :bag}]
+                        (assoc-set m c-bag bag))
+                      m
+                      contain))
+            {}
+            lines)))
+
+(defn find-containing
+  [contained bag]
+  (loop [result #{}
+         todo (contained bag #{})]
+    (if (seq todo)
+      (let [x (first todo)]
+        (recur (conj result x) (into (disj todo x) (contained x #{}))))
+      result)))
 
 (defn part-1
   []
-  nil)
+  (-> (li/read-input "7.txt")
+      (parse-lines)
+      (to-contained)
+      (find-containing "shiny gold")
+      (count)))
 
 (defn part-2
   []
--- test/aoc_clj_2020/test_solution_7.clj
+++ test/aoc_clj_2020/test_solution_7.clj
@@ -2,7 +2,37 @@
   (:require [clojure.test :refer [deftest testing is are]]
             [aoc-clj-2020.solution-7 :as t]))
 
+(def example
+  "light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.")
+
+(deftest test-parse-line
+  (are [line result] (= result (t/parse-line line))
+
+    "light red bags contain 1 bright white bag, 2 muted yellow bags."
+    {:bag "light red"
+     :contain
+     [{:bag "bright white"
+       :amount 1}
+      {:bag "muted yellow"
+       :amount 2}]}
+
+    "faded blue bags contain no other bags."
+    {:bag "faded blue"
+     :contain []}))
+
+(deftest test-find-containing
+  (is (= #{"bright white" "dark orange" "light red" "muted yellow"}
+         (-> example t/parse-lines t/to-contained (t/find-containing "shiny gold")))))
+
 (deftest test-solution-7
   (are [f r] (= (f) r)
-    t/part-1 nil
+    t/part-1 119
     t/part-2 nil))
