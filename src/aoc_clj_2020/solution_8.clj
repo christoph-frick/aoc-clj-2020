4d3aa2206be25d69849f97435d6a731b1633fad6
Christoph Frick <cf@ofnir.net>
2020-12-08 08:29:59 +0100

    Day 8, part 1

--- src/aoc_clj_2020/solution_8.clj
+++ src/aoc_clj_2020/solution_8.clj
@@ -1,11 +1,51 @@
 (ns aoc-clj-2020.solution-8
   (:require [aoc-clj-2020.util.input :as li]
             [aoc-clj-2020.util.parse :as lp]
-            [aoc-clj-2020.util.test :as lt]))
+            [aoc-clj-2020.util.test :as lt]
+            [clojure.string :as str]))
+
+(defn parse-op
+  [s]
+  (let [[_ op ofs] (re-find #"(\w+) ([+-]\d+)" s)]
+    {:op (keyword op) :ofs (lp/atoi ofs)}))
+
+(defn parse-program
+  [lines]
+  (mapv parse-op lines))
+
+(defn op-nop
+  [state _]
+  (update state :current-line inc))
+
+(defn op-acc
+  [state {:keys [ofs]}]
+  (update (op-nop state {}) :acc + ofs))
+
+(defn op-jmp
+  [state {:keys [ofs]}]
+  (update state :current-line + ofs))
+
+(def dispatch
+  {:nop op-nop
+   :acc op-acc
+   :jmp op-jmp})
+
+(defn run-program
+  [ops]
+  (loop [{:keys [seen-lines current-line acc] :as state} {:seen-lines #{}
+                                                          :current-line 0
+                                                          :acc 0}]
+    (if (contains? seen-lines current-line)
+      acc
+      (let [{:keys [op] :as instr} (nth ops current-line)]
+        (recur (update ((dispatch op) state instr) :seen-lines conj current-line))))))
 
 (defn part-1
   []
-  nil)
+  (->>
+   (li/read-lines "8.txt")
+   (parse-program)
+   (run-program)))
 
 (defn part-2
   []
--- test/aoc_clj_2020/test_solution_8.clj
+++ test/aoc_clj_2020/test_solution_8.clj
@@ -2,7 +2,33 @@
   (:require [clojure.test :refer [deftest testing is are]]
             [aoc-clj-2020.solution-8 :as t]))
 
+(def example
+  ["nop +0"
+   "acc +1"
+   "jmp +4"
+   "acc +3"
+   "jmp -3"
+   "acc -99"
+   "acc +1"
+   "jmp -4"
+   "acc +6"])
+
+(deftest test-parse-program
+  (is (= (t/parse-program example)
+         [{:op :nop :ofs 0}
+          {:op :acc :ofs 1}
+          {:op :jmp :ofs 4}
+          {:op :acc :ofs 3}
+          {:op :jmp :ofs -3}
+          {:op :acc :ofs -99}
+          {:op :acc :ofs 1}
+          {:op :jmp :ofs -4}
+          {:op :acc :ofs 6}])))
+
+(deftest test-run-program
+  (is (= 5 (-> example t/parse-program t/run-program))))
+
 (deftest test-solution-8
   (are [f r] (= (f) r)
-    t/part-1 nil
+    t/part-1 1744
     t/part-2 nil))
