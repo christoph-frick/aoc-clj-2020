6bd3c39f72a13998f05e05f892f4f61928256880
Christoph Frick <cf@ofnir.net>
2020-12-09 21:02:59 +0100

    Day 9

--- src/aoc_clj_2020/solution_9.clj
+++ src/aoc_clj_2020/solution_9.clj
@@ -1,12 +1,70 @@
 (ns aoc-clj-2020.solution-9
   (:require [aoc-clj-2020.util.input :as li]
             [aoc-clj-2020.util.parse :as lp]
-            [aoc-clj-2020.util.test :as lt]))
+            [aoc-clj-2020.util.test :as lt]
+            [clojure.string :as str]
+            [clojure.math.combinatorics :as comb]))
+
+(defn parse
+  [s]
+  (mapv lp/atol (str/split-lines s)))
+
+(defn check
+  [preamble xs]
+  (lazy-seq
+   (let  [ps (take preamble xs)
+          n (first (drop preamble xs))]
+     (when n
+       ; optimization: make combinations lazy and find the first
+       ; match instead of doing just a check against the set
+       (let [p-sums (into #{}
+                          (map (partial apply +))
+                          (comb/combinations ps 2))]
+         (cons
+          {(if (contains? p-sums n)
+             :ok
+             :fail)
+           n}
+          (check preamble (rest xs))))))))
+
+(defn attack
+  [preamble xs]
+  (->>
+   xs
+   (check preamble)
+   (filter :fail)
+   (map :fail)))
+
+(defn find-weakness
+  [target xs]
+  (loop [xs xs]
+    (when (seq xs)
+      (let [result (loop [i 2]
+                     (let [frame (take i xs)
+                           sum (apply + frame)]
+                       (if (= sum target)
+                         (apply sorted-set frame)
+                         (when (< sum target)
+                           (recur (inc i))))))]
+
+        (if result
+          result
+          (recur (rest xs)))))))
+
+(defn sum-weakness
+  [sum-set]
+  (apply + ((juxt first last) sum-set)))
 
 (defn part-1
   []
-  nil)
+  (->> (li/read-input "9.txt")
+       (parse)
+       (attack 25)
+       (first)))
 
 (defn part-2
   []
-  nil)
+  (->> (li/read-input "9.txt")
+       (parse)
+       (find-weakness (part-1))
+       (sum-weakness)))
--- test/aoc_clj_2020/test_solution_9.clj
+++ test/aoc_clj_2020/test_solution_9.clj
@@ -2,7 +2,35 @@
   (:require [clojure.test :refer [deftest testing is are]]
             [aoc-clj-2020.solution-9 :as t]))
 
+(def example
+  "35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576")
+
+(deftest test-attack
+  (is (= 127 (->> example (t/parse) (t/attack 5) (first)))))
+
+(deftest test-weakness
+  (is (= 62 (->> example (t/parse) (t/find-weakness 127) (t/sum-weakness)))))
+
 (deftest test-solution-9
   (are [f r] (= (f) r)
-    t/part-1 nil
-    t/part-2 nil))
+    t/part-1 133015568
+    t/part-2 16107959))
