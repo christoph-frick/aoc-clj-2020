590bee36f69d201b3fed4843e6339e3dd62ba5d3
Christoph Frick <cf@ofnir.net>
2020-12-10 13:54:02 +0100

    Day 10, part 1

--- src/aoc_clj_2020/solution_10.clj
+++ src/aoc_clj_2020/solution_10.clj
@@ -3,9 +3,32 @@
             [aoc-clj-2020.util.parse :as lp]
             [aoc-clj-2020.util.test :as lt]))
 
+(defn parse
+  [s]
+  (lp/lines-to lp/atoi s))
+
+(defn differences
+  [lines]
+  ; prefix 0 for the beginning
+  ; always add one 3 for the end
+  (let [sorted (conj (sort lines) 0)]
+    (-> (map (fn [a b]
+               (- b a))
+             sorted
+             (rest sorted))
+        (frequencies)
+        (update 3 inc))))
+
+(defn result
+  [freqs]
+  (* (get freqs 1) (get freqs 3)))
+
 (defn part-1
   []
-  nil)
+  (-> (li/read-input "10.txt")
+      (parse)
+      (differences)
+      (result)))
 
 (defn part-2
   []
--- test/aoc_clj_2020/test_solution_10.clj
+++ test/aoc_clj_2020/test_solution_10.clj
@@ -2,7 +2,58 @@
   (:require [clojure.test :refer [deftest testing is are]]
             [aoc-clj-2020.solution-10 :as t]))
 
+(def example-1
+  "16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4")
+
+(def example-2
+  "28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3")
+
+(deftest test-differences
+  (are [input result] (= result (-> input t/parse t/differences))
+    example-1 {1 7, 3 5}
+    example-2 {1 22, 3 10}))
+
 (deftest test-solution-10
   (are [f r] (= r (f))
-    t/part-1 nil
+    t/part-1 2176
     t/part-2 nil))
