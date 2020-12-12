89b0e3819ed02e1c1b5d8e556df6e6822ffe3619
Christoph Frick <cf@ofnir.net>
2020-12-12 20:52:17 +0100

    Add template for the last day

---
+++ test/aoc_clj_2020/test_solution_25.clj
@@ -0,0 +1,8 @@
+(ns ^:day-24 aoc-clj-2020.test-solution-25
+  (:require [clojure.test :refer [deftest is are]]
+            [aoc-clj-2020.solution-25 :as t]))
+
+(deftest test-solution-24
+  (are [f r] (= r (f))
+    t/part-1 nil
+    t/part-2 nil))
