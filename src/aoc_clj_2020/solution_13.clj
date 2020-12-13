7e09b4d0985175f250590cb42e78899b7f8dd7b1
Christoph Frick <cf@ofnir.net>
2020-12-13 18:10:15 +0100

    WIP Day 13

--- src/aoc_clj_2020/solution_13.clj
+++ src/aoc_clj_2020/solution_13.clj
@@ -1,33 +1,79 @@
 (ns aoc-clj-2020.solution-13
   (:require [aoc-clj-2020.util.input :as li]
             [aoc-clj-2020.util.parse :as lp]
-            [aoc-clj-2020.util.test :as lt]
             [clojure.string :as str]))
 
 (defn parse
   [s]
   (let [[target lines] (str/split-lines s)]
     {:target (lp/atoi target)
-     :lines (mapv lp/atoi (re-seq #"\d+" lines))}))
+     :lines (mapv
+             (fn [s]
+               (if (= s "x")
+                 :x
+                 (lp/atoi s)))
+             (re-seq #"[^,]+" lines))}))
 
 (defn distance
   [target line]
-  (- (* line (int (Math/ceil (/ target line))))
-     target))
+  (let [r (mod target line)]
+    (if (zero? r)
+      r
+      (- line r))))
 
-(defn solve
+(defn minimize-wait
   [{:keys [target lines]}]
   (let [tt (into (sorted-map)
-                 (map (juxt (partial distance target) identity) lines))
+                 (map (juxt (partial distance target) identity) (remove (partial = :x) lines)))
         [line wait] (first tt)]
     (* line wait)))
+
+(defn brute-force
+  [{:keys [lines]}]
+  (let [lines-with-offset (->> (map vector lines (range))
+                               (remove #(= :x (first %))))
+        [step offs] (apply max-key first lines-with-offset)]
+    (first
+     (filter
+      (fn [target]
+        (every? (fn [[line offs]]
+                  (= offs (distance target line)))
+                lines-with-offset))
+      (iterate (partial + step) (- step offs))))))
+
+(defn bezout-identity
+  [a b]
+  (loop [[old-r r] [a b]
+         [old-s s] [1 0]
+         [old-t t] [0 1]]
+    (if-not (zero? r)
+      (let [q (quot old-r r)]
+        (recur [r, (- old-r (* q r))]
+               [s, (- old-s (* q s))]
+               [t, (- old-t (* q t))]))
+      [old-s old-t])))
+
+(defn crt
+  [{:keys [lines]}]
+  (let [mods-and-rems (->> (map vector (range) lines)
+                           (remove #(= :x (second %)))
+                           (map (fn [[a m]] [(if (zero? a) a (- m a)) m]))
+                           (sort-by (comp - first)))]
+    (apply mod
+           (reduce
+            (fn [[a1 n1] [a2 n2]]
+              (let [[m1 m2] (bezout-identity n1 n2)]
+                [(+' (*' a1 m2 n2) (*' a2 m1 n1)) (*' n1 n2)]))
+            mods-and-rems))))
 
 (defn part-1
   []
   (->> (li/read-input "13.txt")
        (parse)
-       (solve)))
+       (minimize-wait)))
 
 (defn part-2
   []
-  nil)
+  (->> (li/read-input "13.txt")
+       (parse)
+       (brute-force)))
--- test/aoc_clj_2020/test_solution_13.clj
+++ test/aoc_clj_2020/test_solution_13.clj
@@ -3,17 +3,49 @@
             [aoc-clj-2020.solution-13 :as t]))
 
 (def example
-  "939
-7,13,x,x,59,x,31,19")
+  "939\n7,13,x,x,59,x,31,19")
+
+(deftest test-distance
+  (are [result target line] (= result (t/distance target line))
+    6 939 7
+    10 939 13
+    5 939 59
+    0 3417 17))
 
 (deftest test-parse
-  (is (= {:target 939 :lines [7, 13, 59, 31, 19]}
+  (is (= {:target 939 :lines [7, 13, :x, :x, 59, :x, 31, 19]}
          (t/parse example))))
 
-(deftest test-solve
-  (is (= 295 (-> example t/parse t/solve))))
+(deftest test-minimize-wait
+  (is (= 295 (-> example t/parse t/minimize-wait))))
+
+(deftest test-bezout-identity
+  (are [result a b] (= result (t/bezout-identity a b))
+    [1 -1] 4 3
+    [5 -2] 5 12
+    [7 -1] 3 20
+    [4 -1] 4 15
+    [5 -2] 5 12))
+
+(deftest test-brute-force
+  (are [s result] (= result (-> s (t/parse) (t/brute-force)))
+    example 1068781
+    "0\n17,x,13,19" 3417
+    "0\n67,7,59,61" 754018
+    "0\n67,x,7,59,61" 779210
+    "0\n67,7,x,59,61" 1261476
+    "0\n1789,37,47,1889" 1202161486))
+
+(deftest test-crt
+  (are [s result] (= result (-> s (t/parse) (t/crt)))
+    example 1068781
+    "0\n17,x,13,19" 3417
+    "0\n67,7,59,61" 754018
+    "0\n67,x,7,59,61" 779210
+    "0\n67,7,x,59,61" 1261476
+    "0\n1789,37,47,1889" 1202161486))
 
 (deftest test-solution-13
   (are [f r] (= r (f))
     t/part-1 5946
-    t/part-2 nil))
+    #_#_t/part-2 nil))
