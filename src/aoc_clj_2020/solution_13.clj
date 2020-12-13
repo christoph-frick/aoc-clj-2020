b2dc971f454d6d00c41f5b1c164ff88f0a5ead53
Christoph Frick <cf@ofnir.net>
2020-12-13 12:31:44 +0100

    Day 13, part 1

--- src/aoc_clj_2020/solution_13.clj
+++ src/aoc_clj_2020/solution_13.clj
@@ -1,11 +1,32 @@
 (ns aoc-clj-2020.solution-13
   (:require [aoc-clj-2020.util.input :as li]
             [aoc-clj-2020.util.parse :as lp]
-            [aoc-clj-2020.util.test :as lt]))
+            [aoc-clj-2020.util.test :as lt]
+            [clojure.string :as str]))
+
+(defn parse
+  [s]
+  (let [[target lines] (str/split-lines s)]
+    {:target (lp/atoi target)
+     :lines (mapv lp/atoi (re-seq #"\d+" lines))}))
+
+(defn distance
+  [target line]
+  (- (* line (int (Math/ceil (/ target line))))
+     target))
+
+(defn solve
+  [{:keys [target lines]}]
+  (let [tt (into (sorted-map)
+                 (map (juxt (partial distance target) identity) lines))
+        [line wait] (first tt)]
+    (* line wait)))
 
 (defn part-1
   []
-  nil)
+  (->> (li/read-input "13.txt")
+       (parse)
+       (solve)))
 
 (defn part-2
   []
--- test/aoc_clj_2020/test_solution_13.clj
+++ test/aoc_clj_2020/test_solution_13.clj
@@ -2,7 +2,18 @@
   (:require [clojure.test :refer [deftest is are]]
             [aoc-clj-2020.solution-13 :as t]))
 
+(def example
+  "939
+7,13,x,x,59,x,31,19")
+
+(deftest test-parse
+  (is (= {:target 939 :lines [7, 13, 59, 31, 19]}
+         (t/parse example))))
+
+(deftest test-solve
+  (is (= 295 (-> example t/parse t/solve))))
+
 (deftest test-solution-13
   (are [f r] (= r (f))
-    t/part-1 nil
+    t/part-1 5946
     t/part-2 nil))
