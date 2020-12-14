66b565c4df6a054c9634f4b1c0e7d1444e846cab
Christoph Frick <cf@ofnir.net>
2020-12-14 09:41:36 +0100

    Day 14, part 1

--- src/aoc_clj_2020/solution_14.clj
+++ src/aoc_clj_2020/solution_14.clj
@@ -1,11 +1,58 @@
 (ns aoc-clj-2020.solution-14
   (:require [aoc-clj-2020.util.input :as li]
             [aoc-clj-2020.util.parse :as lp]
-            [aoc-clj-2020.util.test :as lt]))
+            [aoc-clj-2020.util.test :as lt]
+            [clojure.string :as str]))
+
+(defn parse
+  [s]
+  (lp/lines-to
+   (fn [s]
+     (if-let [[_ mask] (re-find #"^mask = ([X01]+)" s)]
+       [:mask mask]
+       (let [[_ addr value] (re-find #"^mem\[(\d+)\] = (\d+)" s)]
+         [:mem (lp/atoi addr) (lp/atoi value)])))
+   s))
+
+
+(def initial-state
+  {:mask-and (bit-not 0)
+   :mask-or 0
+   :memory {}})
+
+(defn mask-to-bin
+  [s]
+  {:mask-or (Long/parseLong (str/escape s {\X \0}) 2)
+   :mask-and (Long/parseLong (str/escape s {\X \1}) 2)})
+
+(defn apply-mask
+  [{:keys [mask-or mask-and]} value]
+  (->> value
+       (bit-or mask-or)
+       (bit-and mask-and)))
+
+(defn step-1
+  [state [instr & params]]
+  (case instr
+    :mask (let [[mask] params]
+            (merge state (mask-to-bin mask)))
+    :mem (let [[addr value] params]
+           (update state :memory assoc addr (apply-mask state value)))))
+
+(defn run
+  [initial-state instrs]
+  (reduce step-1 initial-state instrs))
+
+(defn sum-memory
+  [{:keys [memory]}]
+  (apply + (vals memory)))
 
 (defn part-1
   []
-  nil)
+  (->> (li/read-input "14.txt")
+       (parse)
+       (run initial-state)
+       (sum-memory)))
 
 (defn part-2
   []
--- test/aoc_clj_2020/test_solution_14.clj
+++ test/aoc_clj_2020/test_solution_14.clj
@@ -2,7 +2,27 @@
   (:require [clojure.test :refer [deftest is are]]
             [aoc-clj-2020.solution-14 :as t]))
 
+(def example
+  "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0")
+
+(deftest test-parse
+  (is (= [[:mask "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"]
+          [:mem 8 11]
+          [:mem 7 101]
+          [:mem 8 0]] (t/parse example))))
+
+(deftest test-mask-to-bin
+  (is (= {:mask-or 64
+          :mask-and 68719476733}
+         (t/mask-to-bin "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"))))
+
+(deftest test-run
+  (is (= 165 (->> example (t/parse) (t/run t/initial-state) (t/sum-memory)))))
+
 (deftest test-solution-14
   (are [f r] (= r (f))
-    t/part-1 nil
+    t/part-1 9615006043476
     t/part-2 nil))
