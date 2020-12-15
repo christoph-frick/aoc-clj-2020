853bc26b8d4fff2600dc1c0eb7b19bbc6c68ce4e
Christoph Frick <cf@ofnir.net>
2020-12-15 11:20:20 +0100

    Day 15, part 1

--- src/aoc_clj_2020/solution_15.clj
+++ src/aoc_clj_2020/solution_15.clj
@@ -3,9 +3,62 @@
             [aoc-clj-2020.util.parse :as lp]
             [aoc-clj-2020.util.test :as lt]))
 
+(defn parse
+  [s]
+  (map lp/atoi (re-seq #"\d+" s)))
+
+(defn setup
+  [game]
+  {:turn (count game)
+   :recent (into {}
+                 (map #(vector %1 (list %2))
+                      game
+                      (iterate inc 1)))
+   :spoken (last game)})
+
+(defn inc-turn
+  [game]
+  (update game :turn inc))
+
+(defn recent
+  [{:keys [recent spoken]}]
+  (when (and (contains? recent spoken)
+             (< 1 (count (recent spoken))))
+    (apply - (get recent spoken))))
+
+(defn update-recent
+  [{:keys [turn spoken] :as game}]
+  (update-in game
+             [:recent spoken]
+             #(take 2 (conj % turn))))
+
+(defn spoken
+  [game nr]
+  (assoc game :spoken nr))
+
+(defn announce
+  [game]
+  (-> game
+      (spoken (or (recent game) 0))
+      update-recent))
+
+(defn step
+  [game]
+  (-> game
+      (inc-turn)
+      (announce)))
+
+(defn run-2020
+  [game]
+  (let [{:keys [spoken]} (first
+                          (filter
+                           #(= 2020 (:turn %))
+                           (iterate step (setup game))))]
+    spoken))
+
 (defn part-1
   []
-  nil)
+  (-> "8,0,17,4,1,12" parse run-2020))
 
 (defn part-2
   []
--- test/aoc_clj_2020/test_solution_15.clj
+++ test/aoc_clj_2020/test_solution_15.clj
@@ -2,6 +2,81 @@
   (:require [clojure.test :refer [deftest is are]]
             [aoc-clj-2020.solution-15 :as t]))
 
+(def example-1
+  "0,3,6")
+
+(deftest test-parse
+  (is (= [0 3 6] (t/parse example-1))))
+
+(deftest test-setup
+  (is (= {:recent {0 '(1)
+                   3 '(2)
+                   6 '(3)}
+          :turn 3
+          :spoken 6}
+         (-> example-1 t/parse t/setup))))
+
+(deftest test-step
+  (is (= [{:recent {0 '(1)
+                    3 '(2)
+                    6 '(3)}
+           :turn 3
+           :spoken 6}
+          {:recent {0 '(4 1)
+                    3 '(2)
+                    6 '(3)}
+           :turn 4
+           :spoken 0}
+          {:recent {0 '(4 1)
+                    3 '(5 2)
+                    6 '(3)}
+           :turn 5
+           :spoken 3}
+          {:recent {0 '(4 1)
+                    3 '(6 5)
+                    6 '(3)}
+           :turn 6
+           :spoken 3}
+          {:recent {0 '(4 1)
+                    1 '(7)
+                    3 '(6 5)
+                    6 '(3)}
+           :turn 7
+           :spoken 1}
+          {:recent {0 '(8 4)
+                    1 '(7)
+                    3 '(6 5)
+                    6 '(3)}
+           :turn 8
+           :spoken 0}
+          {:recent {0 '(8 4)
+                    1 '(7)
+                    3 '(6 5)
+                    4 '(9)
+                    6 '(3)}
+           :turn 9
+           :spoken 4}
+          {:recent {0 '(10 8)
+                    1 '(7)
+                    3 '(6 5)
+                    4 '(9)
+                    6 '(3)}
+           :turn 10
+           :spoken 0}]
+         (let [game (-> example-1 t/parse)]
+           (take 8 (iterate t/step (t/setup game)))))))
+
+(deftest test-run-2020
+  (are [game result] (= result (-> game t/parse t/run-2020))
+    example-1 436
+    "1,3,2" 1
+    "1,3,2" 1
+    "2,1,3" 10
+    "1,2,3" 27
+    "2,3,1" 78
+    "3,2,1" 438
+    "3,1,2" 1836))
+
 (deftest test-solution-15
   (are [f r] (= r (f))
     t/part-1 nil
