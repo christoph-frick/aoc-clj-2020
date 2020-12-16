28f275991cbed260df908d3af4e0c3d797400e2c
Christoph Frick <cf@ofnir.net>
2020-12-16 16:02:41 +0100

    Day 16, part 1

--- src/aoc_clj_2020/solution_16.clj
+++ src/aoc_clj_2020/solution_16.clj
@@ -1,11 +1,56 @@
 (ns aoc-clj-2020.solution-16
   (:require [aoc-clj-2020.util.input :as li]
             [aoc-clj-2020.util.parse :as lp]
-            [aoc-clj-2020.util.test :as lt]))
+            [aoc-clj-2020.util.test :as lt]
+            [clojure.string :as str]))
+
+(defn parse-ticket
+  [s]
+  (mapv lp/atoi
+        (re-seq #"\d+" s)))
+
+(defn parse-rule-ranges
+  [s]
+  (reduce (fn [acc [_ _ s e]]
+            (into acc
+                  (range (lp/atoi s)
+                         (inc (lp/atoi e)))))
+          #{}
+          (re-seq #"((\d+)-(\d+))" s)))
+
+(defn parse-rule
+  [s]
+  (let [[rule-name ranges] (str/split s #":" 2)]
+    [(keyword rule-name) (parse-rule-ranges ranges)]))
+
+(defn parse
+  [s]
+  (let [[rules-s your-ticket-s nearby-tickets-s] (str/split s #"\n\n")]
+    {:rules (into {} (map parse-rule) (str/split-lines rules-s))
+     :your-ticket (parse-ticket (second (str/split-lines your-ticket-s)))
+     :nearby-tickets (mapv parse-ticket (rest (str/split-lines nearby-tickets-s)))}))
+
+(defn combine-rules
+  [{:keys [rules]}]
+  (into #{} (apply concat (vals rules))))
+
+(defn find-simple-violation
+  [combined-rules ticket]
+  (remove combined-rules ticket))
+
+(defn sum-simple-violations
+  [{:keys [nearby-tickets] :as config}]
+  (let [combined-rules (combine-rules config)]
+    (reduce
+     #(apply + %1 (find-simple-violation combined-rules %2))
+     0
+     nearby-tickets)))
 
 (defn part-1
   []
-  nil)
+  (->> (li/read-input "16.txt")
+       (parse)
+       (sum-simple-violations)))
 
 (defn part-2
   []
--- test/aoc_clj_2020/test_solution_16.clj
+++ test/aoc_clj_2020/test_solution_16.clj
@@ -2,7 +2,35 @@
   (:require [clojure.test :refer [deftest is are]]
             [aoc-clj-2020.solution-16 :as t]))
 
+(def example-1
+  "class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12")
+
+(deftest test-parse
+  (is (= {:rules {:class (into #{} (concat (range 1 4) (range 5 8)))
+                  :row (into #{} (concat (range 6 12) (range 33 45)))
+                  :seat (into #{} (concat (range 13 41) (range 45 51)))}
+          :your-ticket [7,1,14]
+          :nearby-tickets [[7,3,47]
+                           [40,4,50]
+                           [55,2,20]
+                           [38,6,12]]}
+         (t/parse example-1))))
+
+(deftest test-sum-simple-violations
+  (is (= 71 (-> example-1 (t/parse) (t/sum-simple-violations)))))
+
 (deftest test-solution-16
   (are [f r] (= r (f))
-    t/part-1 nil
+    t/part-1 26009
     t/part-2 nil))
