d2aae424ab1f117654c8004123c53f7387035f5b
Christoph Frick <cf@ofnir.net>
2020-12-19 17:48:50 +0100

    Day 19, part 1

--- src/aoc_clj_2020/solution_19.clj
+++ src/aoc_clj_2020/solution_19.clj
@@ -1,11 +1,63 @@
 (ns aoc-clj-2020.solution-19
   (:require [aoc-clj-2020.util.input :as li]
             [aoc-clj-2020.util.parse :as lp]
-            [aoc-clj-2020.util.test :as lt]))
+            [aoc-clj-2020.util.test :as lt]
+            [instaparse.core :as insta]
+            [clojure.string :as str]))
+
+(def parser
+  (insta/parser
+   "
+   rules = rule+
+   rule = index <':'> <space> (char|term) <newline>?
+   term = and | or
+   and = (ref <space>?)+
+   or = and <'|'> <space> and
+   newline = '\\n'
+   space = ' '
+   char = <'\"'> #'[a-z]' <'\"'>
+   ref = num
+   index = num
+   num = #'\\d+'
+   "))
+
+(defn parse
+  [s]
+  (->> s
+       (parser)
+       (insta/transform {:num lp/atoi
+                         :or #(into ['|] %&)
+                         :and #(into ['&] (map second) %&)
+                         :rule (fn [[_ index] [_ term]]
+                                 [index term])
+                         :rules #(into {} %&)})))
+
+(defn rules->re
+  [rules]
+  (letfn [(apply-rule [rule]
+            (if (vector? rule)
+              (let [[op & args] rule]
+                (case op
+                  | (str "(" (apply-rule (first args)) "|" (apply-rule (last args)) ")")
+                  & (str "(" (str/join "" (map re args)) ")")))
+              rule))
+          (re [i]
+            (apply-rule (get rules i)))]
+    (re-pattern (re 0))))
+
+(defn run
+  [input]
+  (let [[rules-s messages-s] (lp/split-groups input)
+        rules (parse rules-s)
+        re (rules->re rules)
+        messages (str/split-lines messages-s)]
+    (count
+     (filter (partial re-matches re) messages))))
 
 (defn part-1
   []
-  nil)
+  (-> (li/read-input "19.txt")
+      (run)))
 
 (defn part-2
   []
--- test/aoc_clj_2020/test_solution_19.clj
+++ test/aoc_clj_2020/test_solution_19.clj
@@ -2,7 +2,64 @@
   (:require [clojure.test :refer [deftest is are]]
             [aoc-clj-2020.solution-19 :as t]))
 
+(def example-1
+  "0: 1 2
+1: \"a\"
+2: 1 3 | 3 1
+3: \"b\"")
+
+(def example-2
+  "0: 4 1 5
+1: 2 3 | 3 2
+2: 4 4 | 5 5
+3: 4 5 | 5 4
+4: \"a\"
+5: \"b\"")
+
+(def full-example-1
+  "0: 4 1 5
+1: 2 3 | 3 2
+2: 4 4 | 5 5
+3: 4 5 | 5 4
+4: \"a\"
+5: \"b\"
+
+ababbb
+bababa
+abbbab
+aaabbb
+aaaabbb")
+
+(deftest test-parse
+  (are [input result] (= result (t/parse input))
+    example-1 {0 ['& 1 2]
+               1 "a"
+               2 ['| ['& 1 3] ['& 3 1]]
+               3 "b"}
+    example-2 {0 ['& 4 1 5]
+               1 ['| ['& 2 3] ['& 3 2]]
+               2 ['| ['& 4 4] ['& 5 5]]
+               3 ['| ['& 4 5] ['& 5 4]]
+               4 "a"
+               5 "b"}))
+
+(deftest test-rules->r
+  (are [rules re] (= (str re) (str (-> rules (t/parse) (t/rules->re))))
+    example-1 #"(a((ab)|(ba)))"
+    example-2 #"(a((((aa)|(bb))((ab)|(ba)))|(((ab)|(ba))((aa)|(bb))))b)"))
+
+(deftest test-match
+  (are [rules input result] (= result (boolean (re-matches (-> rules (t/parse) (t/rules->re)) input)))
+    example-2 "ababbb" true
+    example-2 "bababa" false
+    example-2 "abbbab" true
+    example-2 "aaabbb" false
+    example-2 "aaaabbb" false))
+
+(deftest test-run
+  (is (= 2 (t/run full-example-1))))
+
 (deftest test-solution-19
   (are [f r] (= r (f))
-    t/part-1 nil
+    t/part-1 235
     t/part-2 nil))
