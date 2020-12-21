e5792a9deb1d42bb8dc627e28f242ead89f3464b
Christoph Frick <cf@ofnir.net>
2020-12-21 14:31:35 +0100

    Day 21, part 1

--- src/aoc_clj_2020/solution_21.clj
+++ src/aoc_clj_2020/solution_21.clj
@@ -1,11 +1,82 @@
 (ns aoc-clj-2020.solution-21
   (:require [aoc-clj-2020.util.input :as li]
             [aoc-clj-2020.util.parse :as lp]
-            [aoc-clj-2020.util.test :as lt]))
+            [aoc-clj-2020.util.test :as lt]
+            [instaparse.core :as insta]
+            [clojure.set :as set]))
+
+(def line-parser
+  (insta/parser
+   "
+   rule = ingredients allergens
+   ingredients = wordlist
+   allergens = <'(contains '> wordlist <')'>
+   wordlist = (word <space>*)+
+   space = #'[\\s,]+'
+   <word> = #'\\w+'
+   "))
+
+(defn parse-line
+  [line]
+  (->> (line-parser line)
+       (insta/transform
+        {:wordlist (fn [& words] (into #{} words))
+         :rule (fn [& kvs] (into {} kvs))})))
+
+(defn parse
+  [input]
+  (lp/lines-to
+   parse-line
+   input))
+
+(defn solve-allergens
+  [xs]
+  (let [unions (reduce
+                (partial merge-with set/intersection)
+                {}
+                (for [{:keys [ingredients allergens]} xs
+                      allergen allergens]
+                  {allergen ingredients}))
+        count=1? (fn [v] (= 1 (count v)))
+        remove-val (fn [m rm]
+                     (reduce
+                      (fn [m k]
+                        (if (count=1? (m k))
+                          m
+                          (update m k disj rm)))
+                      m
+                      (keys m)))]
+    (loop [result unions]
+      (if (every? count=1? (vals result))
+        (into {} (map (juxt key (comp first val))) result)
+        (let [{done true} (group-by count=1? (vals result))]
+          (recur (reduce remove-val result (map first done))))))))
+
+(defn allergen-free
+  [xs]
+  (let [allergens (->> xs (solve-allergens) (vals) (into #{}))]
+    (into #{}
+          (comp
+           (mapcat :ingredients)
+           (remove allergens))
+          xs)))
+
+(defn ingredient-frequencies
+  [xs]
+  (frequencies (mapcat :ingredients xs)))
+
+(defn count-allergen-free
+  [xs]
+  (apply +
+         (vals
+           (select-keys (ingredient-frequencies xs)
+                        (allergen-free xs)))))
 
 (defn part-1
   []
-  nil)
+  (->> (li/read-input "21.txt")
+       (parse)
+       (count-allergen-free)))
 
 (defn part-2
   []
--- test/aoc_clj_2020/test_solution_21.clj
+++ test/aoc_clj_2020/test_solution_21.clj
@@ -2,7 +2,47 @@
   (:require [clojure.test :refer [deftest is are]]
             [aoc-clj-2020.solution-21 :as t]))
 
+(def data-1
+  "mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+trh fvjkl sbzzf mxmxvkd (contains dairy)
+sqjhc fvjkl (contains soy)
+sqjhc mxmxvkd sbzzf (contains fish)")
+
+(deftest test-parse
+  (is (= [{:ingredients #{"mxmxvkd" "kfcds" "sqjhc" "nhms"} :allergens #{"dairy" "fish"}}
+          {:ingredients #{"trh" "fvjkl" "sbzzf" "mxmxvkd"} :allergens #{"dairy"}}
+          {:ingredients #{"sqjhc" "fvjkl"} :allergens #{"soy"}}
+          {:ingredients #{"sqjhc" "mxmxvkd" "sbzzf"} :allergens #{"fish"}}]
+         (t/parse data-1))))
+
+(deftest test-solve-allergens
+  (is (= {"dairy" "mxmxvkd"
+          "fish" "sqjhc"
+          "soy" "fvjkl"}
+         (->> data-1
+              (t/parse)
+              (t/solve-allergens)))))
+
+(deftest test-allergen-free
+  (is (= #{"kfcds" "nhms" "sbzzf" "trh"}
+         (->> data-1
+              (t/parse)
+              (t/allergen-free)))))
+
+(deftest test-ingredient-frequencies
+  (is (= 2
+         (-> data-1
+             (t/parse)
+             (t/ingredient-frequencies)
+             (get "sbzzf")))))
+
+(deftest test-count-allergen-free
+  (is (= 5
+         (->> data-1
+              (t/parse)
+              (t/count-allergen-free)))))
+
 (deftest test-solution-21
   (are [f r] (= r (f))
-    t/part-1 nil
+    t/part-1 1685
     t/part-2 nil))
