0a72f04f34d894c57579482acd04173a2146b9d1
Christoph Frick <cf@ofnir.net>
2020-12-22 12:07:46 +0100

    Day 22, part 1

--- src/aoc_clj_2020/solution_22.clj
+++ src/aoc_clj_2020/solution_22.clj
@@ -1,11 +1,56 @@
 (ns aoc-clj-2020.solution-22
   (:require [aoc-clj-2020.util.input :as li]
             [aoc-clj-2020.util.parse :as lp]
-            [aoc-clj-2020.util.test :as lt]))
+            [clojure.string :as str])
+  (:import [clojure.lang PersistentQueue]))
+
+(defn parse
+  [s]
+  (->> s
+       (lp/split-groups)
+       (mapv (fn [line]
+               (into (PersistentQueue/EMPTY) (map lp/atoi) (rest (str/split-lines line)))))))
+
+(defn win-round
+  [deck player cards]
+  (update deck player into (if (zero? player) cards (reverse cards))))
+
+(defn draw
+  [deck]
+  ((juxt
+     (partial mapv pop)
+     (partial mapv peek))
+   deck))
+
+(defn step
+  [deck]
+  (let [[deck cards] (draw deck)]
+    (win-round deck (if (apply < cards) 1 0) cards)))
+
+(defn winner
+  [[p1 p2]]
+  (cond
+    (empty? p1) 1
+    (empty? p2) 0))
+
+(defn score
+  [deck player]
+  (apply +
+         (map *
+              (reverse (get deck player))
+              (iterate inc 1))))
+
+(defn run
+  [deck]
+  (if-let [winner (winner deck)]
+    (score deck winner)
+    (recur (step deck))))
 
 (defn part-1
   []
-  nil)
+  (->> (li/read-input "22.txt")
+       (parse)
+       (run)))
 
 (defn part-2
   []
--- test/aoc_clj_2020/test_solution_22.clj
+++ test/aoc_clj_2020/test_solution_22.clj
@@ -2,7 +2,42 @@
   (:require [clojure.test :refer [deftest is are]]
             [aoc-clj-2020.solution-22 :as t]))
 
+(def example-1
+  "Player 1:
+9
+2
+6
+3
+1
+
+Player 2:
+5
+8
+4
+7
+10")
+
+(deftest test-parse
+  (is (= [[9 2 6 3 1]
+          [5 8 4 7 10]]
+         (t/parse example-1))))
+
+(deftest test-step
+  (is (= [[2 6 3 1 9 5]
+          [8 4 7 10]]
+         (-> example-1 t/parse t/step))))
+
+(deftest test-winner
+  (are [deck result] (= result (t/winner deck))
+    [[] [1]] 1
+    [[1] []] 0
+    [[1] [2]] nil))
+
+(deftest test-run
+  (is (= 306
+         (-> example-1 t/parse t/run))))
+
 (deftest test-solution-22
   (are [f r] (= r (f))
-    t/part-1 nil
+    t/part-1 33010
     t/part-2 nil))
