4266a6ce3f80424a63ef6cd8bbc9316dabde672b
Christoph Frick <cf@ofnir.net>
2020-12-22 16:31:29 +0100

    Day 22, part 2

--- src/aoc_clj_2020/solution_22.clj
+++ src/aoc_clj_2020/solution_22.clj
@@ -4,12 +4,28 @@
             [clojure.string :as str])
   (:import [clojure.lang PersistentQueue]))
 
+(defmethod print-method clojure.lang.PersistentQueue
+  [q w]
+  (print-method '< w)
+  (print-method (seq q) w)
+  (print-method '< w))
+
+(defn queue
+  [cards]
+  (into (PersistentQueue/EMPTY) cards))
+
 (defn parse
   [s]
   (->> s
        (lp/split-groups)
        (mapv (fn [line]
-               (into (PersistentQueue/EMPTY) (map lp/atoi) (rest (str/split-lines line)))))))
+               (queue (map lp/atoi (rest (str/split-lines line))))))))
+
+(defn compare-cards
+  [cards]
+  (if (apply < cards)
+    1
+    0))
 
 (defn win-round
   [deck player cards]
@@ -18,20 +34,20 @@
 (defn draw
   [deck]
   ((juxt
-     (partial mapv pop)
-     (partial mapv peek))
+    (partial mapv pop)
+    (partial mapv peek))
    deck))
 
 (defn step
   [deck]
   (let [[deck cards] (draw deck)]
-    (win-round deck (if (apply < cards) 1 0) cards)))
+    (win-round deck (compare-cards cards) cards)))
 
 (defn winner
-  [[p1 p2]]
+  [[d1 d2]]
   (cond
-    (empty? p1) 1
-    (empty? p2) 0))
+    (empty? d1) 1
+    (empty? d2) 0))
 
 (defn score
   [deck player]
@@ -46,6 +62,60 @@
     (score deck winner)
     (recur (step deck))))
 
+(defn init-recursive-game
+  [deck]
+  {:deck deck
+   :seen #{}})
+
+(defn replace-state
+  [[_ & prev*] current]
+  (conj prev* current))
+
+(defn new-recurse
+  [[current :as state] deck cards]
+  (let [deck' (map #(queue (take %2 %1)) deck cards)]
+    (conj
+     (replace-state state (assoc current
+                                 :deck deck
+                                 :loot cards))
+     (init-recursive-game deck'))))
+
+(defn need-recurse?
+  [[d1 d2] [c1 c2]]
+  (and
+   (<= c1 (count d1))
+   (<= c2 (count d2))))
+
+(defn winner-recurse
+  [{:keys [deck seen]}]
+  (if (contains? seen deck)
+    0
+    (winner deck)))
+
+(defn win-recurse
+  [[_ {:keys [deck loot] :as prev} :as state] winner]
+  (replace-state (rest state)
+                 (assoc prev :deck (win-round deck winner loot))))
+
+; FIXME: pretty ugly; might be better to have a proper step fn and make the win condtion just a marker in state
+(defn recursive-run
+  [deck]
+  (loop [state (list (init-recursive-game deck))]
+    (let [{:keys [deck] :as current} (first state)]
+      (if-let [winner (winner-recurse current)]
+        (if (= 1 (count state))
+          (score deck winner)
+          (recur (win-recurse state winner)))
+        (let [prev-deck deck
+              [deck cards] (draw deck)]
+          (recur
+           (if (need-recurse? deck cards)
+             (new-recurse state deck cards)
+             (replace-state state
+                            (-> current
+                                (update :seen conj prev-deck)
+                                (assoc :deck (win-round deck (compare-cards cards) cards)))))))))))
+
 (defn part-1
   []
   (->> (li/read-input "22.txt")
@@ -54,4 +124,6 @@
 
 (defn part-2
   []
-  nil)
+  (->> (li/read-input "22.txt")
+       (parse)
+       (recursive-run)))
--- test/aoc_clj_2020/test_solution_22.clj
+++ test/aoc_clj_2020/test_solution_22.clj
@@ -17,6 +17,16 @@ Player 2:
 7
 10")
 
+(def example-inf
+  "Player 1:
+43
+19
+
+Player 2:
+2
+29
+14")
+
 (deftest test-parse
   (is (= [[9 2 6 3 1]
           [5 8 4 7 10]]
@@ -37,7 +47,20 @@ Player 2:
   (is (= 306
          (-> example-1 t/parse t/run))))
 
+(deftest test-contains-with-queue
+  (is (contains? #{(t/queue [1 2 3])} (t/queue [1 2 3]))))
+
+(deftest test-prevent-infinite-recursion
+  (is (= 105
+         (-> example-inf
+             (t/parse)
+             (t/recursive-run)))))
+
+(deftest test-recursive-run
+  (is (= 291
+         (-> example-1 t/parse t/recursive-run))))
+
 (deftest test-solution-22
   (are [f r] (= r (f))
     t/part-1 33010
-    t/part-2 nil))
+    t/part-2 32769))
