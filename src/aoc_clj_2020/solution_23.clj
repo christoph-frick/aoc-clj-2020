115b9876bfd9d2f6bb1704f2ce22ac9dae3fb922
Christoph Frick <cf@ofnir.net>
2020-12-23 22:19:04 +0100

    Day 23, part 1

--- src/aoc_clj_2020/solution_23.clj
+++ src/aoc_clj_2020/solution_23.clj
@@ -1,11 +1,61 @@
 (ns aoc-clj-2020.solution-23
   (:require [aoc-clj-2020.util.input :as li]
             [aoc-clj-2020.util.parse :as lp]
-            [aoc-clj-2020.util.test :as lt]))
+            [clojure.string :as str]))
+
+; should be done with a linked ring
+
+(defn parse
+  [s]
+  (map lp/atoi (re-seq #"\d" s)))
+
+(defn shift-to
+  [xs fst]
+  (let [[p1 p2] (split-with (partial not= fst) xs)]
+    (into (vec p2) p1)))
+
+(defn shift-list-1
+  [xs]
+  (conj (vec (rest xs))
+        (first xs)))
+
+(defn next-start
+  [start remaining]
+  (let [search (into (sorted-set start) remaining)
+        [mn mx] ((juxt first last) search)]
+    (loop [target (dec start)]
+      (if (contains? search target)
+        target
+        (recur  (if (< target mn) mx (dec target)))))))
+
+(defn step
+  [[start h1 h2 h3 & remaining]]
+  (let [next-start (next-start start remaining)
+        result (concat [next-start]
+                       [h1 h2 h3]
+                       (rest (shift-to (conj remaining start) next-start)))]
+    (-> result
+        (shift-to start)
+        (shift-list-1))))
+
+(defn run
+  [game nr]
+  (if (zero? nr)
+    game
+    (recur (step game) (dec nr))))
+
+(defn solution-1
+  [game]
+  (->> (shift-to game 1)
+       (rest)
+       (str/join "")))
 
 (defn part-1
   []
-  nil)
+  (-> "942387615"
+      (parse)
+      (run 100)
+      (solution-1)))
 
 (defn part-2
   []
--- test/aoc_clj_2020/test_solution_23.clj
+++ test/aoc_clj_2020/test_solution_23.clj
@@ -2,7 +2,39 @@
   (:require [clojure.test :refer [deftest is are]]
             [aoc-clj-2020.solution-23 :as t]))
 
+(def data1
+  "389125467")
+
+(deftest test-parse
+  (is (= [3 8 9 1 2 5 4 6 7]
+         (t/parse data1))))
+
+(deftest test-step-10
+  (is (= [[3 8 9 1 2 5 4 6 7]
+          [2 8 9 1 5 4 6 7 3]
+          [5 4 6 7 8 9 1 3 2]
+          [8 9 1 3 4 6 7 2 5]
+          [4 6 7 9 1 3 2 5 8]
+          [1 3 6 7 9 2 5 8 4]
+          [9 3 6 7 2 5 8 4 1]
+          [2 5 8 3 6 7 4 1 9]
+          [6 7 4 1 5 8 3 9 2]
+          [5 7 4 1 8 3 9 2 6]
+          [8 3 7 4 1 9 2 6 5]]
+         (reductions
+          (fn [game _] (t/step game))
+          (t/parse data1)
+          (range 10)))))
+
+(deftest test-run
+  (are [steps result] (= result (-> data1
+                                    (t/parse)
+                                    (t/run steps)
+                                    (t/solution-1)))
+    10 "92658374"
+    100 "67384529"))
+
 (deftest test-solution-23
   (are [f r] (= r (f))
-    t/part-1 nil
+    t/part-1 "36542897"
     t/part-2 nil))
