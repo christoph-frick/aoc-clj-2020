ba307409bb43a231cfebb3c18571df76bdb11bbd
Christoph Frick <cf@ofnir.net>
2020-12-24 14:42:08 +0100

    Day 24, part 1

--- src/aoc_clj_2020/solution_24.clj
+++ src/aoc_clj_2020/solution_24.clj
@@ -1,11 +1,69 @@
 (ns aoc-clj-2020.solution-24
   (:require [aoc-clj-2020.util.input :as li]
             [aoc-clj-2020.util.parse :as lp]
-            [aoc-clj-2020.util.test :as lt]))
+            [aoc-clj-2020.util.transform :as transform]))
+
+; https://www.redblobgames.com/grids/hexagons/
+
+(defn parse
+  [s]
+  (lp/lines-to
+   (partial re-seq #"(?:se|sw|ne|nw|e|w)")
+   s))
+
+(defn hex
+  ([q r]
+   (hex q r (- (- q) r)))
+  ([q r s]
+   (assert (zero? (+ q r s)))
+   [q r s]))
+
+(defn hex-op
+  ([op a]
+   (mapv op a))
+  ([op a b]
+   (mapv op a b)))
+
+(def ZERO (hex 0 0 0))
+
+(def directions
+  {"e" (hex 1 0)
+   "se" (hex 0 1)
+   "sw" (hex -1 1)
+   "w" (hex -1 0)
+   "nw" (hex 0 -1)
+   "ne" (hex 1 -1)})
+
+(defn grid-flip
+  [grid pos]
+  ((if (contains? grid pos) disj conj) grid pos))
+
+(defn move
+  [pos dir]
+  (hex-op + pos (directions dir)))
+
+(defn instr-to-pos
+  [instr]
+  (reduce move ZERO instr))
+
+(defn solution1
+  [instrs]
+  (reduce
+   (fn [grid instr]
+     (grid-flip grid (instr-to-pos instr)))
+   #{}
+   instrs))
+
+(defn count-flipped
+  [grid]
+  (count grid))
 
 (defn part-1
   []
-  nil)
+  (->> (li/read-input "24.txt")
+       (parse)
+       (solution1)
+       (count-flipped)))
 
 (defn part-2
   []
--- test/aoc_clj_2020/test_solution_24.clj
+++ test/aoc_clj_2020/test_solution_24.clj
@@ -2,7 +2,45 @@
   (:require [clojure.test :refer [deftest is are]]
             [aoc-clj-2020.solution-24 :as t]))
 
+(def ex1
+  "sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew")
+
+(deftest test-parse
+  (is (= [["se" "se" "nw" "ne" "ne" "ne" "w" "se" "e" "sw" "w" "sw" "sw" "w" "ne" "ne" "w" "se" "w" "sw"]]
+         (t/parse "sesenwnenenewseeswwswswwnenewsewsw"))))
+
+(deftest test-instr-to-pos
+  (is (= [-2 1 1]
+         (-> (t/parse "neeswseenwwswnwswswnw") ; ne e sw se e nw w sw nw sw sw nw
+             (first)
+             (t/instr-to-pos)))))
+
+(deftest test-solution1
+  (is (= 10 (-> ex1
+                (t/parse)
+                (t/solution1)
+                (t/count-flipped)))))
+
 (deftest test-solution-24
   (are [f r] (= r (f))
-    t/part-1 nil
+    t/part-1 322
     t/part-2 nil))
