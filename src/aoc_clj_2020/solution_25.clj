c9d75cbc583853bb4dd5e9485fa6307cb1f13d05
Christoph Frick <cf@ofnir.net>
2020-12-25 15:07:59 +0100

    Day 25, part 1

--- src/aoc_clj_2020/solution_25.clj
+++ src/aoc_clj_2020/solution_25.clj
@@ -3,9 +3,41 @@
             [aoc-clj-2020.util.parse :as lp]
             [aoc-clj-2020.util.test :as lt]))
 
+(defn step
+  [subject value]
+  (rem (* value subject) 20201227))
+
+(defn enc-key
+  [subject loop-size]
+  (loop [value 1
+         loop-size loop-size]
+    (if (zero? loop-size)
+      value
+      (recur (step subject value) (dec loop-size)))))
+
+(defn loop-size
+  [subject pub-keys]
+  (loop [loop-size 0
+         values (repeat (count pub-keys) 1)]
+    (if-let [pub-key (first
+                      (filter
+                       (partial apply =)
+                       (map vector
+                            values
+                            pub-keys)))]
+      [(first pub-key) loop-size]
+      (recur (inc loop-size)
+             (map (partial step subject)
+                  values)))))
+
+(def input
+  [9093927 11001876])
+
 (defn part-1
   []
-  nil)
+  (let [[pub-key loop-size] (loop-size 7 input)
+        other-pub-key (first (remove (partial = pub-key) input))]
+    (enc-key other-pub-key loop-size)))
 
 (defn part-2
   []
--- test/aoc_clj_2020/test_solution_25.clj
+++ test/aoc_clj_2020/test_solution_25.clj
@@ -1,8 +1,21 @@
-(ns ^:day-24 aoc-clj-2020.test-solution-25
+(ns ^:day-25 aoc-clj-2020.test-solution-25
   (:require [clojure.test :refer [deftest is are]]
             [aoc-clj-2020.solution-25 :as t]))
 
+(deftest test-enc-key
+  (are [subject loop-size result] (= result (t/enc-key subject loop-size))
+    7 8 5764801
+    7 11 17807724
+    17807724 8 14897079
+    5764801 11 14897079))
+
+(deftest test-loop-size
+  (are [subject pub-key result] (= result (t/loop-size subject pub-key))
+    7 [5764801 17807724] [5764801 8]
+    7 [5764801] [5764801 8]
+    7 [17807724] [17807724 11]))
+
 (deftest test-solution-24
   (are [f r] (= r (f))
-    t/part-1 nil
+    t/part-1 12227206
     t/part-2 nil))
